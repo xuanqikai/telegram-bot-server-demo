fa4d18d747f43955a1348308d532f6128114813e
xuanqikai <824173706@qq.com>
2024-08-23 16:16:55 +0800

    提交

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env?.BOT_TOKEN || "6573484100:AAHw9n7ESNGaY5gjqwNiMsXxqZk8tpI7Vcw";
+
+const bot = new TelegramBot(token!, { polling: true });
+
+bot.onText(/\/start/, (msg: any) => {
+  bot.sendMessage(msg.chat.id, "Hello from TypeScript123-abc!");
+});
+
+console.log('Bot is running...');
+
+
