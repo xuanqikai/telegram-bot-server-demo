4b7374d43a870942e2a5df1bd638225ffe7483a3
xuanqikai <824173706@qq.com>
2024-09-09 22:12:42 +0800

    修改

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const token = process.env?.BOT_TOKEN || "6573484100:AAHw9n7ESNGaY5gjqwNiMsXxqZk8
 const bot = new TelegramBot(token!, { polling: true });
 
 bot.onText(/\/start/, (msg: any) => {
-  bot.sendMessage(msg.chat.id, "Hello from TypeScript123-111!");
+  bot.sendMessage(msg.chat.id, "Hello from TypeScript123-222!");
 });
 
 console.log('Bot is running...');
