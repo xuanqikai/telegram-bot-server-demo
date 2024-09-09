379057e28f7fd324af1172df96008fbef2e7a660
xuanqikai <824173706@qq.com>
2024-09-09 21:41:41 +0800

    测试修改

--- dist/index.js
+++ dist/index.js
@@ -7,11 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const node_telegram_bot_api_1 = __importDefault(require("node-telegram-bot-api"));
 const token = ((_a = process.env) === null || _a === void 0 ? void 0 : _a.BOT_TOKEN) || "6573484100:AAHw9n7ESNGaY5gjqwNiMsXxqZk8tpI7Vcw";
 const bot = new node_telegram_bot_api_1.default(token, { polling: true });
-// 删除 Webhook
-bot.deleteWebHook().then(() => {
-    console.log('Webhook 已删除');
-});
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Hello from TypeScript123!");
+    bot.sendMessage(msg.chat.id, "Hello from TypeScript123-000!");
 });
 console.log('Bot is running...');
