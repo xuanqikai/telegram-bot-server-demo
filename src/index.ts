ba5187014fdc3ca076d494ed9973eb96fe86e79a
xuanqikai <824173706@qq.com>
2024-09-10 17:58:52 +0800

    添加游戏

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env?.BOT_TOKEN || "6573484100:AAHw9n7ESNGaY5gjqwNiMsXxqZk8tpI7Vcw";
+const gameShortName = "testgame";
+const weburl = "https://d2h6lhn2iwtzyf.cloudfront.net/fruit/index.html";
+// const weburl = "https://telegram-test-livid.vercel.app/";
 
 const bot = new TelegramBot(token!, { polling: true });
 
 bot.onText(/\/start/, (msg: any) => {
   bot.sendMessage(msg.chat.id, "Hello from TypeScript123-222!");
+  bot.sendGame(msg.chat.id, gameShortName);
 });
+bot.on('callback_query', async (msg) => {
+  console.log("收到消息callback_query", msg);
+  bot.answerCallbackQuery(msg.id, { url: weburl })
+})
 
 console.log('Bot is running...');
 
