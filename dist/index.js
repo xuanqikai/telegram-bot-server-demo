26037e5d6524102e705474d9b912a7509cffff28
xuanqikai <824173706@qq.com>
2024-09-13 11:18:48 +0800

    测试内联按钮

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -19,12 +10,48 @@ const gameShortName = "testgame";
 const weburl = "https://d2h6lhn2iwtzyf.cloudfront.net/fruit/index.html";
 // const weburl = "https://telegram-test-livid.vercel.app/";
 const bot = new node_telegram_bot_api_1.default(token, { polling: true });
+// bot.onText(/\/start/, (msg: any) => {
+//   bot.sendMessage(msg.chat.id, "Welcome to the game!");
+//   bot.sendGame(msg.chat.id, gameShortName);
+// });
+// bot.on('callback_query', async (msg) => {
+//   console.log("收到消息callback_query", msg);
+//   bot.answerCallbackQuery(msg.id, { url: weburl })
+// })
+// 监听 /start 命令
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Welcome to the game!");
-    bot.sendGame(msg.chat.id, gameShortName);
+    const chatId = msg.chat.id;
+    // 定义一个带有游戏选项的内联键盘
+    const gameOptions = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: '内联-游戏 1', callback_data: 'game1' },
+                    { text: '内联-游戏 2', callback_data: 'game2' }
+                ],
+                [
+                    { text: '内联-游戏 3', callback_data: 'game3' }
+                ]
+            ]
+        }
+    };
+    // 发送带有内联按钮的消息
+    bot.sendMessage(chatId, '请选择一个游戏:', gameOptions);
 });
-bot.on('callback_query', (msg) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("收到消息callback_query", msg);
-    bot.answerCallbackQuery(msg.id, { url: weburl });
-}));
+// 处理按钮点击事件
+bot.on('callback_query', (callbackQuery) => {
+    const msg = callbackQuery.message;
+    const data = callbackQuery.data;
+    if (data === 'game1') {
+        bot.sendMessage(callbackQuery.id, '内联-你选择了游戏 1! 点击链接开始: https://your-game-url-1.com');
+    }
+    else if (data === 'game2') {
+        bot.sendMessage(callbackQuery.id, '内联-你选择了游戏 2! 点击链接开始: https://your-game-url-2.com');
+    }
+    else if (data === 'game3') {
+        bot.sendMessage(callbackQuery.id, '内联-你选择了游戏 3! 点击链接开始: https://your-game-url-3.com');
+    }
+    // 通知 Telegram 操作已成功处理
+    bot.answerCallbackQuery(callbackQuery.id);
+});
 console.log('Bot is running...');
--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,55 @@ const weburl = "https://d2h6lhn2iwtzyf.cloudfront.net/fruit/index.html";
 
 const bot = new TelegramBot(token!, { polling: true });
 
-bot.onText(/\/start/, (msg: any) => {
-  bot.sendMessage(msg.chat.id, "Welcome to the game!");
-  bot.sendGame(msg.chat.id, gameShortName);
+// bot.onText(/\/start/, (msg: any) => {
+//   bot.sendMessage(msg.chat.id, "Welcome to the game!");
+//   bot.sendGame(msg.chat.id, gameShortName);
+// });
+// bot.on('callback_query', async (msg) => {
+//   console.log("收到消息callback_query", msg);
+//   bot.answerCallbackQuery(msg.id, { url: weburl })
+// })
+
+
+// 监听 /start 命令
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  // 定义一个带有游戏选项的内联键盘
+  const gameOptions = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: '内联-游戏 1', callback_data: 'game1' },
+          { text: '内联-游戏 2', callback_data: 'game2' }
+        ],
+        [
+          { text: '内联-游戏 3', callback_data: 'game3' }
+        ]
+      ]
+    }
+  };
+
+  // 发送带有内联按钮的消息
+  bot.sendMessage(chatId, '请选择一个游戏:', gameOptions);
 });
-bot.on('callback_query', async (msg) => {
-  console.log("收到消息callback_query", msg);
-  bot.answerCallbackQuery(msg.id, { url: weburl })
-})
+
+// 处理按钮点击事件
+bot.on('callback_query', (callbackQuery) => {
+  const msg = callbackQuery.message;
+  const data = callbackQuery.data;
+
+  if (data === 'game1') {
+    bot.sendMessage(callbackQuery.id, '内联-你选择了游戏 1! 点击链接开始: https://your-game-url-1.com');
+  } else if (data === 'game2') {
+    bot.sendMessage(callbackQuery.id, '内联-你选择了游戏 2! 点击链接开始: https://your-game-url-2.com');
+  } else if (data === 'game3') {
+    bot.sendMessage(callbackQuery.id, '内联-你选择了游戏 3! 点击链接开始: https://your-game-url-3.com');
+  }
+
+  // 通知 Telegram 操作已成功处理
+  bot.answerCallbackQuery(callbackQuery.id);
+});
 
 console.log('Bot is running...');
 
